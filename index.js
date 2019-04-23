2843908cfb6c176fe9f0dd8278bbe2f679af0906
Saul Shanabrook <s.shanabrook@gmail.com>
2019-04-22 21:21:03 -0400

    Update vega

--- index.js
+++ index.js
@@ -2,32 +2,28 @@
 // This is a VL graph which has a slider to change the 
 // filtered month and then takes the average of the day
 window.VLSpc = {
-  $schema: "https://vega.github.io/schema/vega-lite/v3.0.2.json",
-  config: { view: { height: 300, width: 400 } },
-  data: {
-    name: "initial-data"
-  },
-  encoding: {
-    x: { field: "flight_dayofmonth", type: "ordinal" },
-    y: { aggregate: "average", field: "depdelay", type: "quantitative" }
-  },
-  mark: "line",
-  selection: {
-    month: {
-      bind: { input: "range", max: 12, min: 1, step: 1 },
-      fields: ["flight_month"],
-      type: "single"
+  "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
+  "data": {"url": "data/movies.json"},
+  "mark": "bar",
+  "encoding": {
+    "x": {
+      "bin": true,
+      "field": "IMDB_Rating",
+      "type": "quantitative"
+    },
+    "y": {
+      "aggregate": "count",
+      "type": "quantitative"
     }
-  },
-  transform: [{ filter: { selection: "month" } }]
+  }
 };
 
 window.VSpec = vl.compile(window.VLSpc);
-
 console.log(JSON.stringify(window.VSpec))
 
 // Returns graph in form of https://github.com/vega/vega/tree/master/packages/vega-runtime
 
 window.graph = vega.parse(window.VSpec)
 
+console.log(window.graph)
 console.log(JSON.stringify(window.graph))
